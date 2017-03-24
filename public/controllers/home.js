996eae62dce8fdab5e7d06df318989cb6046110d
Cameron <camdunne@gmail.com>
2017-03-24 17:21:38 -0400

    feat(view): toggle navbar on link and filter archive list

--- public/controllers/home.js
+++ public/controllers/home.js
@@ -1,5 +1,4 @@
 angular.module('sentimently.home',[])
-
 .controller('HomeController', ['$scope', '$http', 'tone', 'render', '$state', function ($scope,$http,tone,render,$state) {
 
   $scope.$state = $state;
@@ -58,9 +57,11 @@ $scope.getArchives = function() {
   .then (function(data) {
     var arrLength = data.data.length;
     $scope.archivesData = [];
-    for (var i=arrLength-1; i>arrLength-16; i--) {
-      if (data.data[i]) {
+    for (var i=arrLength-1; i>arrLength-13; i--) {
+      if (data.data[i] ) {
+        if(!$scope.archivesData.find(item => item.handle === data.data[i].handle)){
         $scope.archivesData.push(data.data[i]);
+        }
       }
     }
     $scope.archivesData.forEach(function(entry) {
