1161029ba18366e6a0c49b3e65e079a627f09e5f
Richard A. Serdici <rserdici@gmail.com>
2017-03-25 15:35:02 -0400

    feat(login): Add login functionality and UI

---
+++ server/config/auth.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'twitterAuth': {
+		'consumerKey': 'pZCiMIFJx1r7lObsb1BT2x47x',
+		'consumerSecret': 'yRxnBTkVZbnX2GDXLW6G9rPnmOEuSqlDrzaPpoXl1cwiCOA7rH',
+		'callbackURL': 'http://localhost:8080/auth/twitter/callback'
+	},
+  'facebookAuth' : {
+    'clientID': '176070402906964',
+    'clientSecret': 'b91494426d1b78f49936db33e989f24e',
+    'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+  }
+}
